4af0512ac103fc4ceaa8dbc44cb6c15b359ba9d7
tocomi <tocomi0112@gmail.com>
2024-03-28 03:02:28 +0900

    feat: handle CORS

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
+import cors from "@fastify/cors"
 import Fastify from "fastify"
 import { getOdaiSuggestions } from "./getOdaiSuggestions"
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000
 const host = "RENDER" in process.env ? "0.0.0.0" : "localhost"
+;(async () => {
+  const fastify = Fastify({
+    logger: true,
+  })
+  await fastify.register(cors, {})
 
-const fastify = Fastify({
-  logger: true,
-})
+  fastify.get<{
+    Querystring: {
+      keyword: string
+    }
+  }>("/", async (request, reply) => {
+    if (!request.query.keyword) {
+      reply.code(400)
+      return { message: "keyword is required" }
+    }
+    const odaiSuggestions = await getOdaiSuggestions(request.query.keyword)
+    reply.send(odaiSuggestions)
+  })
 
-// Declare a route
-fastify.get<{
-  Querystring: {
-    keyword: string
-  }
-}>("/", async (request, reply) => {
-  if (!request.query.keyword) {
-    reply.code(400)
-    return { message: "keyword is required" }
-  }
-  const odaiSuggestions = await getOdaiSuggestions(request.query.keyword)
-  reply.send(odaiSuggestions)
-})
-
-// Run the server!
-fastify.listen({ host, port }, (err, address) => {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  // Server is now listening on ${address}
-})
+  fastify.listen({ host, port }, (err, address) => {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+  })
+})()
